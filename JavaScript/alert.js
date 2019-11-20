45b661182a310a252ddfc8ac5e0165e10c0c8dde
patrick aditya <mpatrick97@gmail.com>
2019-11-20 11:52:23 +0530

    new folder commit

---
+++ JavaScript/alert.js
@@ -0,0 +1,18 @@
+// let new_el = document.querySelector('.buttons');
+//
+// new_el.addEventListener('click', function() {
+//      console.log("click");
+// })
+
+const el  = document.getElementsByTagName('button');
+var news = 0;
+for(i=0; i<el.length;i++)
+{
+    el[i].addEventListener('click', function()
+    {
+        console.log("click");
+        news++;
+        console.log(news);
+        console.log(i);
+    })
+}
