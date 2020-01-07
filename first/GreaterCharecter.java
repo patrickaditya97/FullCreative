9efa45914765ed3ead567cc4a71196b29fb0bf15
Patrick aditya <mpatrick97@gmail.com>
2020-01-07 19:23:24 +0530

    new 01/20

---
+++ first/GreaterCharecter.java
@@ -0,0 +1,33 @@
+package first;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GreaterCharecter
+{
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String string1 = in.next();
+        String string2 = in.next();
+        int strLen = string2.length();
+        int index = 0;
+
+        while(string1.length() >= string2.length())
+        {
+
+
+
+        }
+
+//        for(int i = 0; i<string1.length();i++)
+//        {
+//
+//        }
+
+    }
+
+}
+
